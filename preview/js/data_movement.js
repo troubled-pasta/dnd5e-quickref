f17d6d697c17e8ae43f69fbae770a18d0166eb95
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 08:53:15 +1000

    Update data_movement.js

--- preview/js/data_movement.js
+++ preview/js/data_movement.js
@@ -14,20 +14,6 @@ data_movement = [
         ]
     },
     {
-        title: "Run",
-        icon: "run",
-        subtitle: "Cost: Free Action",
-        description: "Movement cost: 5ft per 5ft moved",
-        reference: "PHB, pg. 190.",
-        bullets: [
-            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
-            "You can move through a nonhostile creature's space.",
-            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
-            "Another creature's space is difficult terrain for you.",
-            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
-        ]
-    },
-    {
         title: "Sprint",
         icon: "sprint",
         subtitle: "Cost: Simple Action",
