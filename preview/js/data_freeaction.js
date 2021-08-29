d87df041985132495fa9c261d346e6e95d0148f8
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 17:32:38 +1000

    Update data_freeaction.js

--- preview/js/data_freeaction.js
+++ preview/js/data_freeaction.js
@@ -10,7 +10,7 @@ data_freeaction = [
             "<b>Blast out of Hands:</b> You knock an item out of your target’s hand, but the target takes no damage. The item can be sent flying, coming to a rest (net hits - 1) meters from the defender.",
             "<b>Dirty Trick:</b> If the attack succeeds with even a single net hit, your opponent takes a –4 dice pool modifier on his next action.",
             "<b>Harder Knock:</b> This Called Shot changes the damage code on Stun-based weapons to Physical with no other change to the DV.",
-            "<b>Knock Down (Melee Only):</b> This attack deals no damage but if successful amd the attacker’s STR + Net Hits exceeds the defender's Physical limit, the attacker knocks the defender to the ground. The attacker may choose to follow (Drop Prone) for free. On a critical glitch, the attacker falls down while the defender stays standing.",
+            "<b>Knock Down</b> <i>(Melee Only)</i><b>:</b> This attack deals no damage but if successful amd the attacker’s STR + Net Hits exceeds the defender's Physical limit, the attacker knocks the defender to the ground. The attacker may choose to follow (Drop Prone) for free. On a critical glitch, the attacker falls down while the defender stays standing.",
             "<b>Shake Up:</b> This shot makes the target lose 5 from their Initiative Score along with their normal damage. If their Initiative Score is dropped below 0, he loses his last Action Phase for this Initiative Pass.",
             "<b>Splitting the Damage:</b> If the defender is wearing sufficient armor, you can choose to call a shot to split damage between the damage tracks.",
             "<b>Trick Shot:</b> The attacker gains a bonus on Intimidation rolls after this shot.",
