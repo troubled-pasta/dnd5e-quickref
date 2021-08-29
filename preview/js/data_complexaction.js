a5a9ab4a7d69cfda1187560b787e49d04c8caa0f
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 17:00:57 +1000

    Update data_complexaction.js

--- preview/js/data_complexaction.js
+++ preview/js/data_complexaction.js
@@ -1,42 +1,28 @@
 data_complexaction = [
     {
-        title: "Fire Automatic Weapon",
-        icon: "ak47",
-        subtitle: "Ranged attack",
-        description: "Fire a readied firearm in full autofire mode",
-        reference: "PHB, pgs. 192,194-195.",
+        title: "Astral projection",
+        icon: "telepathy",
+        subtitle: "Cast time of 1 action",
+        description: "Cast a spell with a casting time of 1 action",
+        reference: "PHB, pg. 192.",
         bullets: [
-            "Certain features, such as the <i>Extra Attack</i> feature of the fighter, allow you to make more than one attack with this action. Each of these attacks is a separate roll and may target different creatures. You may move in between these attacks.",
-            "When you attack with a light melee weapon, you can use a bonus action to attack with your other hand (see the <i>Offhand attack</i> bonus action).",
-            "You may replace one of your melee attacks with a <i>Grapple</i> or a <i>Shove</i>.",
-            "Some conditions give advantage on the attack: attacks against blinded, paralyzed, petrified, restrained, stunned, or unconscious targets; melee attacks against prone targets; attacks by invisible or hidden attackers.",
-            "Some conditions give disadvantage on the attack: attacks against invisible or hidden targets; ranged attacks against prone targets; attacks by blinded, frightened, poisoned, or restrained attackers."
+            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
+            "The target of a spell must be within the spell's range. To target something, you must have a clear path to it, so it can't be behind total cover.",
+            "Spells with material components do not consume the material unless explicitly stated. Unless the cost of a material is given, you can assume that the cost is negligible and the material is simply available in a component pouch.",
+            "Some spells require you to maintain concentration in order to keep their magic active. If you lose concentration, such a spell ends. You lose concentration on a spell if you cast another spell that requires concentration or when you are incapacitated. Each time you take damage, you must make a Constitution saving throw to maintain your concentration. The DC equals 10 or half the damage you take, whichever number is higher."
         ]
     },
     {
-        title: "Fire Mounted/Vehicle Weapon",
-        icon: "jeep",
-        subtitle: "Special ranged attack",
-        description: "Attempt to grab a creature or wrestle with it",
-        reference: "PHB, pg. 195.",
+        title: "Summon/Banish spirit",
+        icon: "ifrit",
+        subtitle: "Cast time of 1 action",
+        description: "Cast a spell with a casting time of 1 action",
+        reference: "PHB, pg. 192.",
         bullets: [
-            "You can use the <i>Attack</i> action to make a special melee attack, a grapple. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
-            "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
-            "Using at least one free hand, you try to seize the target by making a grapple check, a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
-            "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
-        ]
-    },
-    {
-        title: "Melee/Unarmed Attack",
-        icon: "spiked-bat",
-        subtitle: "Melee attack",
-        description: "Shove a creature, either to knock it prone or push it away from you",
-        reference: "PHB, pg. 195.",
-        bullets: [
-            "Using the <i>Attack</i> action, you can make a special melee attack to shove a creature. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
-            "The target of your shove must be no more than one size larger than you, and it must be within your reach.",
-            "You make a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
-            "If you win the contest, you either knock the target prone or push it 5 feet away from you."
+            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
+            "The target of a spell must be within the spell's range. To target something, you must have a clear path to it, so it can't be behind total cover.",
+            "Spells with material components do not consume the material unless explicitly stated. Unless the cost of a material is given, you can assume that the cost is negligible and the material is simply available in a component pouch.",
+            "Some spells require you to maintain concentration in order to keep their magic active. If you lose concentration, such a spell ends. You lose concentration on a spell if you cast another spell that requires concentration or when you are incapacitated. Each time you take damage, you must make a Constitution saving throw to maintain your concentration. The DC equals 10 or half the damage you take, whichever number is higher."
         ]
     },
     {
@@ -53,7 +39,58 @@ data_complexaction = [
         ]
     },
     {
-        title: "Full Defense",
+        title: "Fire automatic weapon",
+        icon: "ak47",
+        subtitle: "Ranged attack",
+        description: "Fire a readied firearm in full autofire mode",
+        reference: "PHB, pgs. 192,194-195.",
+        bullets: [
+            "Certain features, such as the <i>Extra Attack</i> feature of the fighter, allow you to make more than one attack with this action. Each of these attacks is a separate roll and may target different creatures. You may move in between these attacks.",
+            "When you attack with a light melee weapon, you can use a bonus action to attack with your other hand (see the <i>Offhand attack</i> bonus action).",
+            "You may replace one of your melee attacks with a <i>Grapple</i> or a <i>Shove</i>.",
+            "Some conditions give advantage on the attack: attacks against blinded, paralyzed, petrified, restrained, stunned, or unconscious targets; melee attacks against prone targets; attacks by invisible or hidden attackers.",
+            "Some conditions give disadvantage on the attack: attacks against invisible or hidden targets; ranged attacks against prone targets; attacks by blinded, frightened, poisoned, or restrained attackers."
+        ]
+    },
+    {
+        title: "Fire mounted/vehicle weapon",
+        icon: "jeep",
+        subtitle: "Special ranged attack",
+        description: "Attempt to grab a creature or wrestle with it",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "You can use the <i>Attack</i> action to make a special melee attack, a grapple. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
+            "Using at least one free hand, you try to seize the target by making a grapple check, a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
+            "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
+        ]
+    },
+    {
+        title: "Reload",
+        icon: "reload-gun-barrel",
+        subtitle: "Reload a clipless firearm",
+        description: "Equip or unequip a shield",
+        reference: "PHB, pgs. 144-146.",
+        bullets: [
+            "A shield always takes an action to equip or unequip.",
+            "Armor takes several minutes to equip or unequip."
+        ]
+    },
+    {
+        title: "Melee/Unarmed Attack",
+        icon: "spiked-bat",
+        subtitle: "Melee attack",
+        description: "Shove a creature, either to knock it prone or push it away from you",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "Using the <i>Attack</i> action, you can make a special melee attack to shove a creature. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "The target of your shove must be no more than one size larger than you, and it must be within your reach.",
+            "You make a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
+            "If you win the contest, you either knock the target prone or push it 5 feet away from you."
+        ]
+    },
+    {
+        title: "Rigger jump in",
         icon: "surrounded-shield",
         subtitle: "Increase defenses",
         description: "Focus entirely on avoiding attacks",
@@ -61,17 +98,6 @@ data_complexaction = [
         bullets: [
             "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
             "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
-        ]
-    },
-    {
-        title: "Escape Artist",
-        icon: "handcuffs",
-        subtitle: "Escape a grapple",
-        description: "Escape a grapple",
-        reference: "PHB, pg. 195.",
-        bullets: [
-            "To escape a grapple, you must succeed on a Strength (Athletics) or Dexterity (Acrobatics) check contested by the grappler's Strength (Athletics) check.",
-            "Escaping other conditions that restrain you (such as manacles) may require a Dexterity or Strength check, as specified by the condition."
         ]
     },
     {
@@ -95,31 +121,6 @@ data_complexaction = [
         bullets: [
             "You can interact with one object for free during your turn (such as drawing a weapon or opening a door). If you want to interact with a second object, use this action.",
             "When an object requires your action for its use, you also take this action."
-        ]
-    },
-    {
-        title: "Reload",
-        icon: "reload-gun-barrel",
-        subtitle: "Reload a clipless firearm",
-        description: "Equip or unequip a shield",
-        reference: "PHB, pgs. 144-146.",
-        bullets: [
-            "A shield always takes an action to equip or unequip.",
-            "Armor takes several minutes to equip or unequip."
-        ]
-    },
-    {
-        title: "Hide",
-        icon: "cowled",
-        subtitle: "tbc",
-        description: "Attempt to hide",
-        reference: "PHB, pg. 192.",
-        bullets: [
-            "You can't hide from a creature that can see you. You must have total cover, be in a heavily obscured area, be invisible, or otherwise block the enemy's vision.",
-            "If you make noise (such as shouting a warning or knocking over a vase), you give away your position.",
-            "When you try to hide, make a Dexterity (Stealth) check and note the result. Until you are discovered or you stop hiding, that check's total is contested by the Wisdom (Perception) check of any creature that actively searches for signs of your presence.",
-            "A creature notices you even if it isn't searching unless your Stealth check is higher than its Passive Perception.",
-            "Out of combat, you may also use a Dexterity (Stealth) check for acts like concealing yourself from enemies, slinking past guards, slipping away without being noticed, or sneaking up on someone without being seen or heard."
         ]
     },
     {
