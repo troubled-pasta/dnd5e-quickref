51b92808361de42e1c5ebb45eaad14b4e06e0519
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 10:49:20 +1000

    Update data_simpleaction.js

--- preview/js/data_simpleaction.js
+++ preview/js/data_simpleaction.js
@@ -1,8 +1,8 @@
 data_simpleaction = [
     {
-        title: "Offhand Attack",
+        title: "Ready weapon",
         icon: "crossed-swords",
-        subtitle: "Use with the Attack action",
+        subtitle: "Ready a weapon for combat",
         description: "Attack with your off hand",
         reference: "PHB, pgs. 192,194-195.",
         bullets: [
@@ -13,9 +13,9 @@ data_simpleaction = [
         ]
     },
     {
-        title: "Cast a spell",
+        title: "Change gun mode",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 bonus action",
+        subtitle: "Change the firing type of a firearm",
         description: "Cast a spell with a casting time of 1 bonus action",
         reference: "PHB, pg. 192.",
         bullets: [
@@ -24,10 +24,50 @@ data_simpleaction = [
         ]
     },
     {
-        title: "Use class feature",
-        icon: "embrassed-energy",
-        subtitle: "Some features use bonus actions",
+        title: "Remove clip (Manually)",
+        icon: "machine-gun-magazine",
+        subtitle: "Remove a clip from a ready firearm",
         description: "Use a racial or class feature that uses a bonus action",
+        reference: "See class page for more information.",
+        bullets: [
+
+        ]
+    },
+    {
+        title: "Insert clip",
+        icon: "machine-gun-magazine",
+        subtitle: "Insert a fresh clip into a ready firearm",
+        description: "A character may insert a fresh clip into a ready firearm if the previous one has been ejected",
+        reference: "See class page for more information.",
+        bullets: [
+
+        ]
+    },
+    {
+        title: "Quick draw",
+        icon: "bandit",
+        subtitle: "Draw and immediately fire a firearm",
+        description: "A character may insert a fresh clip into a ready firearm if the previous one has been ejected",
+        reference: "See class page for more information.",
+        bullets: [
+
+        ]
+    },
+    {
+        title: "Fire weapon",
+        icon: "bandit",
+        subtitle: "Fire a readied weapon",
+        description: "A character may insert a fresh clip into a ready firearm if the previous one has been ejected",
+        reference: "See class page for more information.",
+        bullets: [
+
+        ]
+    },
+        {
+        title: "Throw weapon",
+        icon: "bandit",
+        subtitle: "Throw a readied weapon",
+        description: "A character may insert a fresh clip into a ready firearm if the previous one has been ejected",
         reference: "See class page for more information.",
         bullets: [
 
