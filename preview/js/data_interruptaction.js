8294e3252ff9eb3c769fe894ab03c95c844677b2
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 19:29:06 +1000

    Create data_interruptaction.js

---
+++ preview/js/data_interruptaction.js
@@ -0,0 +1,68 @@
+data_interruptaction = [
+    {
+        title: "Block",
+        icon: "vr-headset",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on avoiding attacks",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+        ]
+    },
+    {
+        title: "Dodge",
+        icon: "dodging",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on avoiding attacks",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+        ]
+    },
+    {
+        title: "Full defense",
+        icon: "vr-headset",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on avoiding attacks",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+        ]
+    },
+    {
+        title: "Drop prone",
+        icon: "falling",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on avoiding attacks",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+        ]
+    },
+    {
+        title: "Intercept",
+        icon: "vr-headset",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on avoiding attacks",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+        ]
+    },
+    {
+        title: "Parry",
+        icon: "vr-headset",
+        subtitle: "Increase defenses",
+        description: "Focus entirely on avoiding attacks",
+        reference: "PHB, pg. 192.",
+        bullets: [
+            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
+            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+        ]
+    }
+]
