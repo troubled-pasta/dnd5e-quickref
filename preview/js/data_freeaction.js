3670ef308244ef915b7b486884ecceaf5c3340f2
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 17:28:20 +1000

    Update data_freeaction.js

--- preview/js/data_freeaction.js
+++ preview/js/data_freeaction.js
@@ -1,18 +1,21 @@
 data_freeaction = [
     {
         title: "Call a shot",
-        icon: "crossed-swords",
+        icon: "bullseye",
         subtitle: "Aim for a vulnerable target",
         description: "A character may call a shot (aim for a vulnerable portion of a target) with this Free Action. This action must be combined with a Fire Weapon, Throw Weapon, or Melee Attack Action",
         reference: "SR5, pg. 195.",
         bullets: [
             "All called shots incur a –4 dice pool penalty and cost a Free Action in addition to their basic attack action.",
-            "Blast out of Hands: You knock an item out of your target’s hand, but the target takes no damage. Attacks intended to disarm suffer the normal –4 dice pool modifier for Called Shots as well as any other situational modifiers that apply such as wounds, lighting, or range. The defender rolls as normal. The item can be sent flying, coming to a rest (net hits - 1) meters from the defender. The item should travel in a direction away from the shooter.",
-            "Dirty Trick: Whether it’s shooting the plasterboard to kick up dust or actually kicking dirt in your opponent’s eyes, there are all sorts of dirty tricks you can do to get an advantage in a not-so-fair fight. If the attack succeeds with even a single net hit, your opponent takes a –4 dice pool modifier on his next action due to the distraction or disturbance.",
-            "Harder Knock: Whether it’s shooting a gel round into an opponent’s face or punching someone in the throat with your fist, sometimes you want an attack designed to do Stun damage to do Physical instead. This Called Shot changes the damage code on Stun-based weapons to Physical with no other change to the DV.",
-            "Knock Down (Melee Only): An attacking character may attempt to knock his opponent to the ground by bowling him over, sweeping his feet out from under him, pulling him off balance, or any similar maneuver. The attacker must declare his intention to perform a knockdown attack during the Declare Actions part of the Action Phase. The attacker makes a melee attack as normal. If he scores more hits than the defender, compare the attacker’s Strength + Net Hits to the defender’s Physical limit. If the attacker’s total exceeds the defender’s Physical limit, the attacker knocks the defender to the ground. This knockdown attack causes no damage to the target (except maybe to their pride). The attacker chooses whether to follow the defender to the ground (a free Drop Prone action) or stay on his feet—unless he glitches, in which case he falls as well. On a critical glitch, the attacker falls down while the defender stays standing.",
-            "Shake Up: Sometimes you just need to slow down an opponent and keep them out of the fight. These shots represent the intentional shot past the ear or skipping rounds off the ground to keep an opponent on their toes and on the run. This shot makes the target lose 5 from their Initiative Score along with their normal damage. If his Initiative Score is dropped below 0, he loses his last Action Phase for this Initiative Pass. Even if the defender completely resists all damage, as long as the shot hit, they still lose the Initiative Score."
-        ]
+            "Blast out of Hands:  You knock an item out of your target’s hand, but the target takes no damage. The item can be sent flying, coming to a rest (net hits - 1) meters from the defender.",
+            "Dirty Trick:  If the attack succeeds with even a single net hit, your opponent takes a –4 dice pool modifier on his next action.",
+            "Harder Knock: This Called Shot changes the damage code on Stun-based weapons to Physical with no other change to the DV.",
+            "Knock Down (Melee Only): This attack deals no damage but if successful amd the attacker’s STR + Net Hits exceeds the defender's Physical limit, the attacker knocks the defender to the ground. The attacker may choose to follow (Drop Prone) for free. On a critical glitch, the attacker falls down while the defender stays standing.",
+            "Shake Up: . This shot makes the target lose 5 from their Initiative Score along with their normal damage. If their Initiative Score is dropped below 0, he loses his last Action Phase for this Initiative Pass.",
+            "Splitting the Damage: If the defender is wearing sufficient armor, you can choose to call a shot to split damage between the damage tracks.",
+            "Trick Shot: The attacker gains a bonus on Intimidation rolls after this shot.",
+            "Vitals: Targeting a vital spot with a called shot gives you an extra +2 DV on the attack."
+           ]
     },
     {
         title: "Change Smartgun mode",
