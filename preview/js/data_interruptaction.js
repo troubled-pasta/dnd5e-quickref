596dbda91631f4f640d8668842140af49bc2ade0
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 22:55:56 +1000

    Update data_interruptaction.js

--- preview/js/data_interruptaction.js
+++ preview/js/data_interruptaction.js
@@ -2,67 +2,71 @@ data_interruptaction = [
     {
         title: "Block",
         icon: "shield-reflect",
-        subtitle: "subtitle",
-        description: "description",
-        reference: "PHB, pg. 192.",
+        subtitle: "-5 Initiative Score",
+        description: "A character may choose to use her own unarmed combat skill to block incoming melee attacks as an Interrupt Action (see Defending in Combat, p. 188)",
+        reference: "SR5, pgs. 168, 188.",
         bullets: [
-            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
-            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+            "By decreasing their Initiative Score, the defending character can add their Unarmed Combat skill to their defense test.",
+            "This is a one-time add, unlike going on Full Defense, which lasts for an entire Combat Turn. Bonus die, such as die from a relevant weapon foci, can be added to this test."
         ]
     },
     {
         title: "Dodge",
-        icon: "dodging",
-        subtitle: "subtitle",
-        description: "description",
-        reference: "PHB, pg. 192.",
+        icon: "acrobatic",
+        subtitle: "-5 Initiative Score",
+        description: "A character may choose to use their own skill to dodge incoming attacks as an Interrupt Action (see Defending in Combat, p. 188).",
+        reference: "SR5, pgs. 168, 188.",
         bullets: [
-            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
-            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
-        ]
-    },
-    {
-        title: "Full defense",
-        icon: "surrounded-shield",
-        subtitle: "subtitle",
-        description: "description",
-        reference: "PHB, pg. 192.",
-        bullets: [
-            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
-            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+            "By decreasing their Initiative Score, the defending character can add their Gymnastics skill to the defense test.",
+            "This is a one-time add, unlike going on Full Defense which lasts for an entire Combat Turn."
         ]
     },
     {
         title: "Drop prone",
         icon: "falling",
-        subtitle: "subtitle",
-        description: "description",
-        reference: "PHB, pg. 192.",
+        subtitle: "-5 Initiative Score",
+        description: "A character who has already used their Free Action may choose to drop prone when caught by suppressive fire using this action (see Suppressive Fire, p. 179)",
+        reference: "SR5, pgs. 168, 179.",
         bullets: [
-            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
-            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+            "The defending character doesn’t need to make the REA + Edge test but is considered prone on their next Action Phase.",
+            "If the suppressive fire continues, the defending character needs to use a <b>Stand Up</b> Simple Action to get back up, risking getting shot, or fight from the ground.",
+            "If the suppressive fire has stopped the prone character still needs to use a <b>Stand Up</b> Simple Action to get back up."
         ]
     },
     {
         title: "Intercept",
-        icon: "vr-headset",
-        subtitle: "subtitle",
-        description: "description",
-        reference: "PHB, pg. 192.",
+        icon: "dodging",
+        subtitle: "-5 Initiative Score",
+        description: "A character may take an Interrupt Action to intercept an opponent who attempts to move past them (within 1 + Reach meter(s)) or break out of melee combat (see Interception, p. 194).",
+        reference: "SR5, pgs. 168, 194.",
         bullets: [
-            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
-            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+            "The Intercept Action allows a character to make a melee attack out of turn.", 
+            "If the character does not have enough Initiative Score left that Action Phase they cannot intercept."
+        ]
+    },
+     {
+        title: "Parry",
+        icon: "cross-mark",
+        subtitle: "-5 Initiative Score",
+        description: "A character may choose to use their own melee weapon skill to deflect incoming melee attacks as an Interrupt Action (see Defending in Combat, p. 188).",
+        reference: "SR5, pgs. 168, 188.",
+        bullets: [
+            "By decreasing their Initiative Score, the defending character can add their melee weapon skill to their defense test.",
+            "This is a one-time add, unlike going on Full Defense, which lasts for an entire Combat Turn."
         ]
     },
     {
-        title: "Parry",
-        icon: "vr-headset",
-        subtitle: "subtitle",
-        description: "description",
-        reference: "PHB, pg. 192.",
+        title: "Full defense",
+        icon: "surrounded-shield",
+        subtitle: "-10 Initiative Score",
+        description: "A character may choose to focus on deliberately avoiding incoming attacks as an Interrupt Action (see Defending in Combat, p. 188).",
+        reference: "SR5, pgs. 168, 188.",
         bullets: [
-            "Until the start of your next turn, any attack roll made against you has disadvantage if you can see the attacker, and you make Dexterity saving throws with advantage.",
-            "You lose this benefit if you are <i>incapacitated</i> or if your speed drops to 0."
+            "This action adds the character’s WIL to their Defense tests for the entire Combat Turn.",
+            "Full Defense actions may be taken at any time, even before the character’s Action Phase as an Interrupt Action, as long as the character is not surprised.",
+            "Characters may only go on Full Defense if they have enough Initiative Score left in that Combat Turn."
+            "The bonus gained in this manner is kept until the end of the Combat Turn so a character can choose to fight defensively early in the round by decreasing their Initiative Score to increase their defense.",
+            "This bonus is also cumulative with other Interrupt Actions."
         ]
     }
 ]
