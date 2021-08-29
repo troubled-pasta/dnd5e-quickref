bedfd1804c7a7d9470f100db9357166d181d893c
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-29 18:19:47 +1000

    Update data_freeaction.js

--- preview/js/data_freeaction.js
+++ preview/js/data_freeaction.js
@@ -7,10 +7,10 @@ data_freeaction = [
         reference: "SR5, pg. 195.",
         bullets: [
             "<i>All called shots incur a –4 dice pool penalty and cost a Free Action in addition to their basic attack action.</i>",
-            "<b>Blast out of Hands:</b> You knock an item out of your target’s hand, but the target takes no damage. The item can be sent flying, coming to a rest (net hits - 1) meters from the defender.",
+            "<b>Blast out of Hands:</b> You knock an item out of your target’s hand, but the target takes no damage. The item can be sent flying, coming to a rest (Net Hits - 1) meters from the defender.",
             "<b>Dirty Trick:</b> If the attack succeeds with even a single net hit, your opponent takes a –4 dice pool modifier on his next action.",
             "<b>Harder Knock:</b> This Called Shot changes the damage code on Stun-based weapons to Physical with no other change to the DV.",
-            "<b>Knock Down</b> <i>(Melee Only)</i><b>:</b> This attack deals no damage but if successful amd the attacker’s STR + Net Hits exceeds the defender's Physical limit, the attacker knocks the defender to the ground. The attacker may choose to follow (Drop Prone) for free. On a critical glitch, the attacker falls down while the defender stays standing.",
+            "<b>Knock Down:</b> <i>(Melee Only)</i> This attack deals no damage but if successful and the attacker’s STR + Net Hits exceeds the defender's Physical limit, the attacker knocks the defender to the ground. The attacker may choose to follow (Drop Prone) for free. On a critical glitch, the attacker falls down while the defender stays standing.",
             "<b>Shake Up:</b> This shot makes the target lose 5 from their Initiative Score along with their normal damage. If their Initiative Score is dropped below 0, he loses his last Action Phase for this Initiative Pass.",
             "<b>Splitting the Damage:</b> If the defender is wearing sufficient armor, you can choose to call a shot to split damage between the damage tracks.",
             "<b>Trick Shot:</b> The attacker gains a bonus on Intimidation rolls after this shot.",
@@ -18,35 +18,55 @@ data_freeaction = [
            ]
     },
     {
-        title: "Change Smartgun mode",
-        icon: "multiple-targets",
-        subtitle: "Change firing mode",
-        description: "Execute the reaction specified by your Ready action",
-        reference: "PHB, pg. 193.",
-        bullets: [
-            "Trigger: specified by your <i>Ready</i> action."
-        ]
-    },
-    {
         title: "Change linked device mode",
-        icon: "circuitry",
-        subtitle: "tbc",
-        description: "Cast a spell with a casting time of 1 reaction",
-        reference: "PHB, pg. 192.",
+        icon: "toggles",
+        subtitle: "Switch modes on any device.",
+        description: "A character may use a Free Action to activate, deactivate, or switch the mode on any device that he is linked to by a direct neural interface through either a wired or wireless link.",
+        reference: "SR5, pg. 163.",
         bullets: [
-            "Trigger: specified by the spell.",
+            "This includes activating cyberware, changing a smartgun’s firing mode, changing a smartlinked shotgun’s choke, deactivating thermographic vision, switching a commlink to hidden mode, turning a device’s wireless functionality off.",
             "For further details, see the <i>Cast a spell</i> action."
         ]
     },
     {
         title: "Drop object",
         icon: "drop-weapon",
-        subtitle: "tbc",
-        description: "Cast a spell with a casting time of 1 reaction",
-        reference: "PHB, pg. 192.",
+        subtitle: "Drop a held object",
+        description: "A character may drop a held object as a Free Action.",
+        reference: "SR5, pg. 163.",
         bullets: [
-            "Trigger: specified by the spell.",
-            "For further details, see the <i>Cast a spell</i> action."
+            "If they are holding an object in each hand, they may drop both objects as a single Free Action.",
+            "Dropped items may suffer damage from being dropped if they are fragile or dropped in a hostile environment (GM's discretion)."
+        ]
+    },
+    {
+        title: "Eject smartgun clip",
+        icon: "drop-weapon",
+        subtitle: "Eject a linked firearm's clip",
+        description: "A character linked to a ready smartgun may use a mental command to eject the weapon’s clip.",
+        reference: "SR5, pg. 164.",
+        bullets: [
+            "It still takes a separate Simple Action to insert a new, fresh clip (see Smartgun System, SR5 p. 433)."
+        ]
+    },
+    {
+        title: "Gesture",
+        icon: "mute",
+        subtitle: "Use gestures to communicate meaning",
+        description: "A character may communicate with a few quick gestures as a Free Action.",
+        reference: "SR5, pg. 164.",
+        bullets: [
+            "Characters unfamiliar with the gestures may make an INT (2) Test to determine what the gesture means."
+        ]
+    },
+    {
+        title: "Multiple attacks",
+        icon: "two-shadows",
+        subtitle: "Attack multiple targets",
+        description: "A character may use a Free Action to attack multiple targets in a single action by splitting their dice pool.",
+        reference: "SR5, pg. 196.",
+        bullets: [
+            "This action must be combined with a Fire Weapon Action, Throw Weapon Action, Melee Attack Action, Reckless Spellcasting, or Cast Spell Action."
         ]
     }
 ]
