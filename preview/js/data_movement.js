2ecd9c679943d47576757b259904e6be437bd69a
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-08-30 19:18:34 +1000

    Update data_movement.js

--- preview/js/data_movement.js
+++ preview/js/data_movement.js
@@ -52,13 +52,30 @@ data_movement = [
             ]
     },
     {
-        title: "Swim",
-        icon: "at-sea",
+        title: "Rapel",
+        icon: "mountain-climbing",
         subtitle: "Cost: Complex Action",
-        description: "description",
-        reference: "PHB, pg. 182.",
+        description: "Climbing, either assisted by ropes and harnesses or unassisted without any gear, is a Complex Acion.",
+        reference: "SR5, pgs. 134, 448.",
         bullets: [
-            ""
+            "The Gymnastics skill is applied to both assisted and unassisted climbing tasks. Assisted climbing is accomplished with specialized gear such as ropes and harnesses (see p. 448). Assisted climbing is easier and safer, but requires more preparation. Unassisted climbing may be done on the fly, because you climb without any gear.",
+            "The number of hits you get on your Gymnastics + STR [Physical] Test determines how far you can move vertically or laterally during your climb, as noted on the Climbing Table.",
+            "The more difficult the climb, the more dice pool modifiers you get, which is also noted on the Climbing Table",
+            "<table><thead><tr><th>Situation</th><th>Movement distance</th></tr></thead><tbody><tr><td>Climbing up (Assisted)<br></td><td>1 Meter per Hit</td></tr><tr><td>Climbing down (Assisted)<br></td><td>2 Meters</td></tr><tr><td>Climbing horizontally<br></td><td>1 Meter per 2 hits</td></tr><tr><td>Climbing upside down (assisted)</td><td>1 Meter per 3 Hits</td></tr><tr><td>Climbing up (unassisted)</td><td>1 Meter per 2 Hits</td></tr><tr><td>Climbing down (unassisted)</td><td>1 meter per hit</td></tr><tr><th>Situation</th><th>Dice Pool Modifier</th></tr><tr><td>Assisted Climbing</td><td>+2</td></tr><tr><th>Surface Is...</th><th>Dice Pool Modifier</th></tr><tr><td>Easily climbable (chain link fence)</td><td>+1</td></tr><tr><td>Broken (tree, loose stone wall)</td><td>+0</td></tr><tr><td>Flat (brick wall, side of old building) </td><td>-3</td></tr><tr><td>Sheer (metal wall, seamless stone)</td><td>-5</td></tr><tr><td>Slippery or wet</td><td>-2</td></tr><tr><td>Greased or gel-treated</td><td>-4</td></tr></tbody></table>"
+            ]
+    },
+    {
+        title: "Swim",
+        icon: "swim",
+        subtitle: "Cost: Complex Action",
+        description: "This skill determines the character’s ability to swim in various bodies of water. The skill level affects the distance and speed at which a character can swim.",
+        reference: "SR5, pgs. 134, 137.",
+        bullets: [
+            "Swimming is a Complex Action. A swimming character can move a number of meters in a Combat Turn equal to the average of their AGI and STR.",
+            "You can take a Simple Action to “sprint” with a Swimming + STR [Physical] Test, adding 1 meter per hit, or 2 meters per hit if you’re an elf or a troll. Swimming on the surface of water or under it does not affect this rate of speed.",
+            "You can tread water for a number of minutes equal to your STR rating. At the end of this time, make a Simple Swimming + STR [Physical] (2) Test. Failure means you slip and take 1 box of Stun damage that you can’t resist. If you have a flotation device, double the time between tests.",
+            "A character can hold their breath for 60 seconds (20 Combat Turns) before needing to make a test. A character needing to hold their breath longer can make a Swimming + WIL Test. Each net hit extends the clock by 15 seconds (5 Combat Turns).",
+            "As with the Running skill, swimmers suffer from fatigue after an extended time (Fatigue Damage, p. 172)."
             ]
     },
     {
