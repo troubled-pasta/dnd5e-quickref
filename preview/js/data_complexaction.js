60d72e3226bd7e35ba561af70a2571073694be5d
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-09-04 14:06:25 +1000

    Update data_complexaction.js

--- preview/js/data_complexaction.js
+++ preview/js/data_complexaction.js
@@ -178,9 +178,9 @@ data_complexaction = [
         description: "All Illegal actions increase Overwatch Score by the target's net hits on its defense test. When your Overwatch Score hits 40, the jig is up. The nastiness that follows is called Convergence (see SR5 pg. 232).",
         reference: "SR5, pgs. 164, 232, 237-244.",
         bullets: [
-		"<b>Enter/Exit Host:</b> <i>Marks reqired: 2</i><br>Test: None.<br>You enter a host that you have a mark on and your icon appears there, or you leave a host you’re already in. A host allows anyone to enter if they’ve got a mark. The host might not be so welcoming once you’re inside, of course, and some IC has the ability to keep you trapped in a host until you can break out.",
+		"<b>Enter/Exit Host:</b> <i>Marks reqired: 2</i><br>You enter a host that you have a mark on and your icon appears there, or you leave a host you’re already in. A host allows anyone to enter if they’ve got a mark. The host might not be so welcoming once you’re inside, of course, and some IC has the ability to keep you trapped in a host until you can break out.",
 		
-		"<b>Grid Hop:</b> <i>Marks reqired: 2</i><br>Test: None.<br>To do this, you must have access to your destination grid. If you do not have access, you can use Brute Force or Hack on the Fly to hop to another grid illegally. If you’re inside a host, you need to leave the host before you can hop to another grid.",
+		"<b>Grid Hop:</b> <i>Marks reqired: 2</i><br>To do this, you must have access to your destination grid. If you do not have access, you can use Brute Force or Hack on the Fly to hop to another grid illegally. If you’re inside a host, you need to leave the host before you can hop to another grid.",
 		
 		"<b>Hide:</b> (Illegal) <i>Marks reqired: 2</i><br>Test: Electronic Warfare + INT [Sleaze] v. INT + Data Processing.<br>You can use this action to make a target lose you. If you succeed, the target stops spotting you and needs to perform a new Matrix Perception action against you if it wants to find you again. You can’t hide from an icon that has a mark on you, so you’ll need to clear those before you can try this action.",
 		
