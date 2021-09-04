c21481e3bfe4de9473f0a8b4f350a963858f0b15
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-09-04 21:00:51 +1000

    Update data_environment.js

--- preview/js/data_environment.js
+++ preview/js/data_environment.js
@@ -2,11 +2,14 @@ data_environment_environmental = [
     {
         title: "Glare",
         icon: "eclipse-flare",
-        subtitle: "",
-        description: "",
-        reference: "",
+        subtitle: "Glare-related penalties",
+        description: "Glare inflicts negative modifiers on some attack rolls. There are a variety of methods a runner can use to mitigate Glare modifiers",
+        reference: "SR5, pg. 173-176.",
         bullets: [
-            ""
+            "<table><thead><tr><th>Glare</th><th>Modifier</th></tr></thead><tbody><tr><td>No Glare</td><td>0</td></tr><tr><td>Weak Glare</td><td>-1</td></tr><tr><td>Moderate Glare</td><td>-3</td></tr><tr><td>Blinding Glare</td><td>-6</td></tr><tr><td>Combined with any other condition already at a -6 penalty</td><td>-10</td></tr></tbody></table>",
+            "Two or more conditions (wind, range, etc.) at the -6 level incur a 10 penalty.",
+            "Flare compensation reduces Glare modifiers by two categories, thermographic vision and sunglasses reduce Glare modifiers by one category.",
+            "Ultrasound eliminates the penalty in any light conditions (but only within 50 meters)."
         ]
     },
     {
