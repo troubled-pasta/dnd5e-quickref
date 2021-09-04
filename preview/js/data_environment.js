bc03774d87129e1e522cc16a3b850bc2087924b3
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-09-04 19:19:35 +1000

    Update data_environment.js

--- preview/js/data_environment.js
+++ preview/js/data_environment.js
@@ -3,40 +3,40 @@ data_environment_obscurance = [
         title: "Clear",
         icon: "sight-disabled",
         subtitle: "No Dice Pool Modifier",
-        description: "Dim light, patchy fog, moderate foliage",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Creatures have <b>disadvantage on Wisdom (Perception)</b> checks that rely on sight."
+            ""
         ]
     },
     {
         title: "Lightly obscured",
         icon: "sight-disabled",
         subtitle: "-1 Dice Pool Modifier",
-        description: "Darkness, opaque fog, dense foliage",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "A creature in a heavily obscured area effectively suffers from the <b>blinded condition</b>."
+            ""
         ]
     },
     {
         title: "Moderately obscured",
         icon: "sight-disabled",
         subtitle: "-3 Dice Pool Modifier",
-        description: "Darkness, opaque fog, dense foliage",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "A creature in a heavily obscured area effectively suffers from the <b>blinded condition</b>."
+            ""
         ]
     },
     {
         title: "Heavily obscured",
         icon: "sight-disabled",
         subtitle: "-6 Dice Pool Modifier",
-        description: "Darkness, opaque fog, dense foliage",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "A creature in a heavily obscured area effectively suffers from the <b>blinded condition</b>."
+            ""
         ]
     }
 ]
@@ -46,45 +46,40 @@ data_environment_light = [
         title: "Full light",
         icon: "star-pupil",
         subtitle: "No Dice Pool Modifier",
-        description: "Bright light lets most creatures see normally",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Gloomy days still provide bright light, as do torches, lanterns, fires, and other sources of illumination within a specific radius."
+            ""
         ]
     },
     {
         title: "Partial light",
         icon: "semi-closed-eye",
         subtitle: "-1 Dice Pool Modifier",
-        description: "Dim light, also called shadows",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Creates a <b>lightly obscured</b> area.",
-            "An area of dim light is usually a boundary between a source of bright light, such as a torch, and surrounding darkness.",
-            "The soft light of twilight and dawn also counts as dim light. A particularly brilliant full moon might bathe the land in dim light."
+            ""
         ]
     },
     {
         title: "Dim light",
         icon: "semi-closed-eye",
         subtitle: "-3 Dice Pool Modifier",
-        description: "Dim light, also called shadows",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Creates a <b>lightly obscured</b> area.",
-            "An area of dim light is usually a boundary between a source of bright light, such as a torch, and surrounding darkness.",
-            "The soft light of twilight and dawn also counts as dim light. A particularly brilliant full moon might bathe the land in dim light."
+            ""
         ]
     },
     {
         title: "Total Darkness",
         icon: "worried-eyes",
         subtitle: "-6 Dice Pool Modifier",
-        description: "Darkness creates a heavily obscured area",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Creates a <b>heavily obscured</b> area.",
-            "Characters face darkness outdoors at night (even most moonlit nights), within the confines of an unlit dungeon or a subterranean vault, or in an area of magical darkness."
+            ""
         ]
     }
 ]
@@ -94,45 +89,40 @@ data_environment_glare = [
         title: "No glare",
         icon: "star-pupil",
         subtitle: "No Dice Pool Modifier",
-        description: "Bright light lets most creatures see normally",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Gloomy days still provide bright light, as do torches, lanterns, fires, and other sources of illumination within a specific radius."
+            ""
         ]
     },
     {
         title: "Weak glare",
         icon: "star-pupil",
         subtitle: "-1 Dice Pool Modifier",
-        description: "Dim light, also called shadows",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Creates a <b>lightly obscured</b> area.",
-            "An area of dim light is usually a boundary between a source of bright light, such as a torch, and surrounding darkness.",
-            "The soft light of twilight and dawn also counts as dim light. A particularly brilliant full moon might bathe the land in dim light."
+            ""
         ]
     },
     {
         title: "Moderate glare",
         icon: "star-pupil",
         subtitle: "-3 Dice Pool Modifier",
-        description: "Dim light, also called shadows",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Creates a <b>lightly obscured</b> area.",
-            "An area of dim light is usually a boundary between a source of bright light, such as a torch, and surrounding darkness.",
-            "The soft light of twilight and dawn also counts as dim light. A particularly brilliant full moon might bathe the land in dim light."
+            ""
         ]
     },
     {
         title: "Blinding glare",
         icon: "star-pupil",
         subtitle: "-6 Dice Pool Modifier",
-        description: "Darkness creates a heavily obscured area",
-        reference: "PHB, pg. 183.",
+        description: "",
+        reference: "",
         bullets: [
-            "Creates a <b>heavily obscured</b> area.",
-            "Characters face darkness outdoors at night (even most moonlit nights), within the confines of an unlit dungeon or a subterranean vault, or in an area of magical darkness."
+            ""
         ]
     }
 ]
@@ -141,46 +131,42 @@ data_environment_glare = [
 data_environment_vision = [
     {
         title: "Astral perception",
-        icon: "one-eyed",
-        subtitle: "Perceive without sight",
-        description: "Perceive your surroundings without relying on sight, within a certain radius",
-        reference: "PHB, pg. 183.",
+        icon: "gaze",
+        subtitle: "subtitle",
+        description: "",
+        reference: "",
         bullets: [
-            "Creatures without eyes, such as oozes, and creatures with echolocation or heightened senses, such as bats and true dragons, have this sense."
+            ""
         ]
     },
     {
         title: "Low-light vision",
-        icon: "semi-closed-eye",
-        subtitle: "Limited vision in darkness",
-        description: "A creature with Darkvision can see better in the dark or low light conditions, within a certain radius",
-        reference: "PHB, pgs. 183-184.",
+        icon: "",
+        subtitle: "",
+        description: "",
+        reference: "",
         bullets: [
-            "Within a specified range, a creature with darkvision can <b>see in darkness as if the darkness were dim light</b>, so areas of darkness are only lightly obscured as far as that creature is concerned.",
-            "However, the creature can’t discern color in darkness, only shades of gray.",
-            "Many creatures in the worlds of D&D, especially those that dwell underground, have darkvision."
+            ""
         ]
     },
     {
         title: "Thermographic vision",
-        icon: "eye-shield",
-        subtitle: "See in darkness",
-        description: "A creature with truesight can see everything in its true form, independent of the environment",
-        reference: "PHB, pg. 184.",
+        icon: "",
+        subtitle: "",
+        description: "",
+        reference: "",
         bullets: [
-            "A creature with truesight can, out to a specific range, see in normal and magical darkness, see invisible creatures and objects, automatically detect visual illusions and succeed on saving throws against them, and perceives the original form of a shapechanger or a creature that is transformed by magic.",
-            "Furthermore, the creature can see into the Ethereal Plane."
+            ""
         ]
     },
     {
         title: "Ultrasound",
-        icon: "eye-shield",
-        subtitle: "See in darkness",
-        description: "A creature with truesight can see everything in its true form, independent of the environment",
-        reference: "PHB, pg. 184.",
+        icon: "",
+        subtitle: "",
+        description: "",
+        reference: "",
         bullets: [
-            "A creature with truesight can, out to a specific range, see in normal and magical darkness, see invisible creatures and objects, automatically detect visual illusions and succeed on saving throws against them, and perceives the original form of a shapechanger or a creature that is transformed by magic.",
-            "Furthermore, the creature can see into the Ethereal Plane."
+            ""
         ]
     }
 ]
@@ -188,37 +174,32 @@ data_environment_vision = [
 data_environment_cover = [
     {
         title: "Half cover",
-        icon: "broken-shield",
-        subtitle: "Low wall, furniture",
-        description: "A target has half cover if an obstacle blocks at least half of its body",
-        reference: "PHB, pg. 196.",
+        icon: "",
+        subtitle: "",
+        description: "",
+        reference: "",
         bullets: [
-            "The obstacle might be a low wall, a large piece of furniture, a narrow tree trunk, or a creature, whether that creature is an enemy or a friend.",
-            "A target with half cover has a <b>+2 bonus to AC and Dexterity saving throws</b>.",
-            " If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+            ""
         ]
     },
     {
         title: "Three-quarters cover",
-        icon: "cracked-shield",
-        subtitle: "Portcullis, arrow slit",
-        description: "A target has three-quarters cover if about three-quarters of it is covered by an obstacle",
-        reference: "PHB, pg. 196.",
+        icon: "",
+        subtitle: "",
+        description: "",
+        reference: "",
         bullets: [
-            "The obstacle might be a portcullis, an arrow slit, or a thick tree trunk.",
-            "A target with three-quarters cover has a <b>+5 bonus to AC and Dexterity saving throws</b>.",
-            " If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+            ""
         ]
     },
     {
         title: "Full cover",
-        icon: "shield",
-        subtitle: "Completely concealed",
-        description: "A target has total cover if it is completely concealed by an obstacle",
-        reference: "PHB, pg. 196.",
+        icon: "",
+        subtitle: "",
+        description: "",
+        reference: "",
         bullets: [
-            "A target with total cover <b>can’t be targeted directly</b> by an attack or a spell, although some spells can reach such a target by including it in an area of effect.",
-            "If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+            ""
         ]
     }
 ]
