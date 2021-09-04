a36c31dc6ccf6bed953f1d29c155324a5304d469
troubled-pasta <89680565+troubled-pasta@users.noreply.github.com>
2021-09-04 14:29:40 +1000

    Update data_complexaction.js

--- preview/js/data_complexaction.js
+++ preview/js/data_complexaction.js
@@ -196,6 +196,7 @@ data_complexaction = [
         description: "Almost all Resonance actions cause Fading, a mental drain on the technomancer akin to spell drain for Magicians. Fading is resisted with RES + WIL.",
         reference: "SR5, pgs. 164, 250-251, 254-256.",
         bullets: [
+		"All sprites show up as illegal activity. You can increase your sprites’ longevity by registering them with the Matrix. This process takes a number of hours equal to the sprite’s Level.",
 		"<b>Compile Sprite:</b><br>Test: Compiling + RES [Level] v. (Sprite Level).<br>When you compile a sprite, you choose a Level for the sprite, up to twice your RES rating. The higher the Level, the more powerful the sprite. For every net hit on the Compiling test, you get one task from the sprite from the list of compiled sprite tasks (pg 254-256). Compiling sprites causes Fading of 2 DV per hit (not net hit) it gets in its defense test, with a minimum of 2 DV. This Fading is Stun damage, unless the sprite’s Level is greater than your RES, in which case it’s Physical damage. You can only have one compiled sprite at any given time.",
 		
 		"<b>Decompile Sprite:</b><br>Test: Decompiling + RES [Level] v. (Sprite Rating) + (Compiler’s RES).<br> Every net hit reduces the sprite’s owed tasks by 1. If the sprite is reduced to 0 tasks owed to its compiler, it returns to the Resonance on its next action. This causes Fading equal to 2 DV per hit (not net hit) the sprite rolls, with a minimum DV of 2.",
